fa727ac81790fa25920d88eb7990a7994efae69a
rafaelcregis <145412855+rafaelcregis@users.noreply.github.com>
2024-08-09 20:09:27 -0300

    Add files via upload

--- dbCantina.sql
+++ dbCantina.sql
@@ -18,3 +18,34 @@ salario decimal(9,2) default 0 check(salario >=0),
 nascimento date,
 telCelular char(10),
 primary key(codFunc));
+
+create table tbFornecedores
+codForn int not null auto_increment,
+nome varchar(100) not null,
+email varchar(100) not null,
+cnpj char(17) not null unique,
+primary key(codForn));
+
+create table tbUsuarios(
+codUsu int not null auto_increment,
+nome varchar(25) not null unique,
+senha varchar(10) not null,
+codFunc int not null,
+primary key(codUsu),
+foreign key(codFunc)references tbFuncionarios(codFunc));
+
+create table tbProdutos(
+codProd int not null auto_increment,
+descricao varchar (100),
+quantidade int,
+valor decimal(9,2),
+validade date,
+dataEntrada date,
+horaEntrada time,
+codForn int not null,
+primary key(codProd),
+foreign key(codForn) references tbFornecedores(codForn));
+
+--visualizando a estrutura das tabelas
+desc tbFuncionarios;
+desc tbUsuarios;
