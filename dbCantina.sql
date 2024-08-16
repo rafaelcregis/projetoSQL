9589ca41eb8d4f131a0c46c4198a0a1e13a3a9d9
rafaelcregis <145412855+rafaelcregis@users.noreply.github.com>
2024-08-16 20:20:18 -0300

    Add files via upload

--- dbCantina.sql
+++ dbCantina.sql
@@ -90,12 +90,14 @@ insert into tbClientes(nome,email,telCelular)values('Cecilia Maria','cecimaria@g
 insert into tbUsuarios(nome,senha,codFunc)values('jose.firmino','123456',1);
 insert into tbUsuarios(nome,senha,codFunc)values('maria.estela','121212',2);
 
-insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,codForn)values('Caderno Brochura',500,1000.00,'2050/18/12','2024/08/16','19:50:00',1);
+insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,codForn)values('Caderno Brochura',500,1000.00,'2050/12/18','2024/08/16','19:50:00',1);
 insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,codForn)values('Caneta Bic',800,800.00,'2027/10/07','2024/08/16','19:53:00',2);
 insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,codForn)values('Lapis Preto',2000,500.00,'2070/01/01','2024/08/16','19:54:00',2);
 insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,codForn)values('Borracha Branca',700,200.00,'2030/05/08','2024/08/16','19:55:00',1);	
 
 insert into tbVendas(dataVenda,horaVenda,quantidade,codUsu,codCli,codProd)values('2024/08/16','19:57:00',10,1,2,4);
+insert into tbVendas(dataVenda,horaVenda,quantidade,codUsu,codCli,codProd)values('2024/08/16','19:58:00',2,2,3,1);
+insert into tbVendas(dataVenda,horaVenda,quantidade,codUsu,codCli,codProd)values('2024/08/16','19:59:00',1,2,3,2);
 
 -- visualizando os registros das tabelas
 
@@ -105,3 +107,11 @@ select * from tbClientes;
 select * from tbUsuarios;
 select * from tbProdutos;
 select * from tbVendas;
+
+-- alterando os registros das tabelas
+
+update tbProdutos set descricao = "Coxinha", valor = 7.00 where codProd =1; 
+
+-- visualizando depois das alterações
+
+select * from tbProdutos;
